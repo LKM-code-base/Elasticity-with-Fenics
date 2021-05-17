5b99d635cc65a21366160236894185649286b1a0
Sebastian Glane <glane@tu-berlin.de>
2021-05-17 16:25:19 +0200

    Added method converting elasticity coefficients

--- source/auxiliary_methods.py
+++ source/auxiliary_methods.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import dolfin as dlfn
+from math import isfinite, sqrt
+from enum import Enum, auto
 
-__all__ = ["boundary_normal"]
+__all__ = ["boundary_normal", "compute_elasticity_coefficients"]
+
+
+class ElasticModuli(Enum):
+    YoungsModulus = auto()
+    PoissonRatio = auto()
+    ShearModulus = auto()
+    BulkModulus = auto()
+    FirstLameParameter = auto()
+    SecondLameParameter = ShearModulus
 
 
 def boundary_normal(mesh, facet_markers, bndry_id):
@@ -43,7 +54,7 @@ def boundary_normal(mesh, facet_markers, bndry_id):
                         raise ValueError("Midpoint connection vector is not tangential to boundary facets.")
                 normal_vectors.append(current_normal)
                 midpoints.append(current_midpoint)
-    
+
     assert len(normal_vectors) > 0, "Boundary id is not marked in MeshFunction"
     assert len(midpoints) == len(normal_vectors)
 
@@ -51,3 +62,189 @@ def boundary_normal(mesh, facet_markers, bndry_id):
     normal = normal_vectors[0]
 
     return tuple(normal[d] for d in range(dim))
+
+
+def compute_elasticity_coefficients(**kwargs):
+    """
+    Compute various elasticity coefficients from the input dictionary.
+    For example,
+
+        compute_elasticity_coefficients(E=210.0, nu=0.3)
+
+    returns
+
+        {BulkModulus: ,
+         ShearModulus: ,
+         YoungsModulus: , PoissonsRatio: ,
+         FirstLameParameter: ,
+         SecondLameParameter: }
+    """
+    assert len(kwargs) == 2, "Only two elastic material parameters are independent"
+    youngs_modulus = None
+    poissons_ratio = None
+    shear_modulus = None
+    bulk_modulus = None
+    first_lame = None
+
+    return_dict = dict()
+
+    for key, value in kwargs.items():
+        assert isinstance(value, float)
+        assert isfinite(value)
+        if key == "E":
+            assert value > 0.0
+            youngs_modulus = value
+            return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        elif key == "nu":
+            assert value < 0.5
+            poissons_ratio = value
+            return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        elif key == "K":
+            assert value > 0.0
+            bulk_modulus = value
+            return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+        elif key == "G" or key == "mu":
+            assert value > 0.0
+            shear_modulus = value
+            return_dict[ElasticModuli.ShearModulus] = shear_modulus
+            return_dict[ElasticModuli.SecondLameParameter] = shear_modulus
+
+        elif key == "lmbda" or key.lower() == "firstlame":
+            first_lame = value
+            return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+        else:  # pragma: no cover
+            raise ValueError("Unexpected value key in function input.")
+
+    if youngs_modulus is not None and poissons_ratio is not None:
+        shear_modulus = youngs_modulus
+        shear_modulus /= 2. * (1. + poissons_ratio)
+        return_dict[ElasticModuli.ShearModulus] = shear_modulus
+
+        bulk_modulus = youngs_modulus
+        bulk_modulus /= 3.0 * (1. - 2. * poissons_ratio)
+        return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+        first_lame = youngs_modulus * poissons_ratio
+        first_lame /= ((1. + poissons_ratio) * (1. - 2. * poissons_ratio))
+        return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+    elif youngs_modulus is not None and shear_modulus is not None:
+        poissons_ratio = youngs_modulus / 2. / shear_modulus - 1.0
+        return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        bulk_modulus = youngs_modulus * shear_modulus
+        bulk_modulus /= 3.0 * (3. * shear_modulus - youngs_modulus)
+        return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+        first_lame = shear_modulus * (youngs_modulus - 2. * shear_modulus)
+        first_lame /= 3. * shear_modulus - youngs_modulus
+        return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+    elif youngs_modulus is not None and bulk_modulus is not None:
+        poissons_ratio = 3. * bulk_modulus - youngs_modulus
+        poissons_ratio /= 6. * bulk_modulus
+        return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        shear_modulus = 3. * bulk_modulus * youngs_modulus
+        shear_modulus /= 9. * bulk_modulus - youngs_modulus
+        return_dict[ElasticModuli.ShearModulus] = shear_modulus
+
+        first_lame = 3. * bulk_modulus * (3. * bulk_modulus - youngs_modulus)
+        first_lame /= 9. * bulk_modulus - youngs_modulus
+        return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+    elif youngs_modulus is not None and first_lame is not None:
+        R = sqrt(youngs_modulus**2 + 9. * first_lame**2
+                 + 2.0 * youngs_modulus * first_lame)
+        poissons_ratio = 2.0 * first_lame
+        poissons_ratio /= youngs_modulus + first_lame + R
+        return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        shear_modulus = (youngs_modulus - 3. * first_lame + R) / 4.0
+        return_dict[ElasticModuli.ShearModulus] = shear_modulus
+
+        bulk_modulus = (youngs_modulus + 3. * first_lame + R) / 6.0
+        return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+    elif poissons_ratio is not None and shear_modulus is not None:
+        youngs_modulus = 2.0 * shear_modulus * (1.0 + poissons_ratio)
+        return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        bulk_modulus = 2.0 * shear_modulus * (1.0 + poissons_ratio)
+        bulk_modulus /= 3.0 * (1. - 2. * poissons_ratio)
+        return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+        first_lame = 2.0 * shear_modulus * poissons_ratio
+        first_lame /= (1. - 2. * poissons_ratio)
+        return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+    elif poissons_ratio is not None and bulk_modulus is not None:
+        youngs_modulus = 3.0 * bulk_modulus * (1.0 - 2.0 * poissons_ratio)
+        return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        shear_modulus = 3.0 * bulk_modulus * (1.0 - 2.0 * poissons_ratio)
+        shear_modulus /= 2.0 * (1.0 + poissons_ratio)
+        return_dict[ElasticModuli.ShearModulus] = shear_modulus
+
+        first_lame = 3.0 * bulk_modulus * poissons_ratio
+        first_lame /= 1. + poissons_ratio
+        return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+    elif poissons_ratio is not None and first_lame is not None:
+        youngs_modulus = first_lame * (1.0 + poissons_ratio) * (1.0 - 2.0 * poissons_ratio)
+        youngs_modulus /= poissons_ratio
+        return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        shear_modulus = first_lame * (1.0 - 2.0 * poissons_ratio)
+        shear_modulus /= 2.0 * poissons_ratio
+        return_dict[ElasticModuli.ShearModulus] = shear_modulus
+
+        bulk_modulus = first_lame * (1.0 + poissons_ratio)
+        bulk_modulus /= 3.0 * poissons_ratio
+        return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+    elif shear_modulus is not None and bulk_modulus is not None:
+        youngs_modulus = 9.0 * bulk_modulus * shear_modulus
+        youngs_modulus /= 3.0 * bulk_modulus + shear_modulus
+        return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        poissons_ratio = 3.0 * bulk_modulus - 2.0 * shear_modulus
+        poissons_ratio /= 2.0 * (3.0 * bulk_modulus + shear_modulus)
+        return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        first_lame = bulk_modulus - 2.0 * shear_modulus / 3.0
+        return_dict[ElasticModuli.FirstLameParameter] = first_lame
+
+    elif shear_modulus is not None and first_lame is not None:
+        youngs_modulus = shear_modulus * (3.0 * first_lame + 2.0 * shear_modulus)
+        youngs_modulus /= shear_modulus + first_lame
+        return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        poissons_ratio = first_lame / 2.0 / (first_lame + shear_modulus)
+        return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        bulk_modulus = first_lame + 2.0 * shear_modulus / 3.0
+        return_dict[ElasticModuli.BulkModulus] = bulk_modulus
+
+    elif bulk_modulus is not None and first_lame is not None:
+        youngs_modulus = 9.0 * bulk_modulus * (bulk_modulus - first_lame)
+        youngs_modulus /= 3.0 * bulk_modulus - first_lame
+        return_dict[ElasticModuli.YoungsModulus] = youngs_modulus
+
+        poissons_ratio = first_lame / (3.0 * bulk_modulus - first_lame)
+        return_dict[ElasticModuli.PoissonRatio] = poissons_ratio
+
+        shear_modulus = 3.0 * (bulk_modulus - first_lame) / 2.0
+        return_dict[ElasticModuli.ShearModulus] = shear_modulus
+    else:  # pragma: no cover
+        raise RuntimeError()
+
+    assert len(return_dict) == 5
+    assert all(isinstance(x, float) for x in return_dict.values())
+    assert all(isfinite(x) for x in return_dict.values())
+
+    return return_dict
--- tests/test_auxiliary_methods.py
+++ tests/test_auxiliary_methods.py
@@ -5,11 +5,24 @@ from grid_generator import HyperCubeBoundaryMarkers
 from grid_generator import hyper_simplex
 from grid_generator import HyperSimplexBoundaryMarkers
 from auxiliary_methods import boundary_normal
+from auxiliary_methods import ElasticModuli
+from auxiliary_methods import compute_elasticity_coefficients
 import math
 
 
-def compare_tuples(a, b):
-    assert all(abs(x-y) < 5.0e-15 for x,y in zip(a,b))
+def compare_tuples(a, b, tol=1.0e-15):
+    assert isinstance(a, tuple)
+    assert isinstance(b, tuple)
+    assert isinstance(tol, float) and tol > 0.0
+    assert all(abs(x-y) < tol for x, y in zip(a, b))
+
+
+def compare_dicts(a, b, tol=1.0e-15):
+    assert isinstance(a, dict)
+    assert isinstance(b, dict)
+    assert isinstance(tol, float) and tol > 0.0
+    assert all(key in b for key in a.keys())
+    assert all(abs(a[key] - b[key]) < tol for key, value in a.items())
 
 
 def test_boundary_normal():
@@ -43,22 +56,72 @@ def test_boundary_normal():
     for normal, bndry_id in zip(normal_vectors, boundary_ids):
         computed_normal = boundary_normal(mesh, boundary_markers, bndry_id)
         compare_tuples(normal, computed_normal)
-        
+
     # simplex: two-dimensional case
     mesh, boundary_markers = hyper_simplex(2, 2)
 
     sqrt_2 = math.sqrt(2.0)
     normal_vectors = [(-1.0, 0.0),
                       (0.0, -1.0),
-                      (1.0 / sqrt_2, 1.0 / sqrt_2 )]
+                      (1.0 / sqrt_2, 1.0 / sqrt_2)]
     boundary_ids = (HyperSimplexBoundaryMarkers.left.value,
                     HyperSimplexBoundaryMarkers.bottom.value,
                     HyperSimplexBoundaryMarkers.diagonal.value)
 
     for normal, bndry_id in zip(normal_vectors, boundary_ids):
         computed_normal = boundary_normal(mesh, boundary_markers, bndry_id)
-        compare_tuples(normal, computed_normal)
+        compare_tuples(normal, computed_normal, tol=5.0e-15)
+
+
+def test_elasticity_coefficients():
+    E = 210.0
+    nu = 0.3
+    lmbda = nu * E / (1. + nu) / (1. - 2. * nu)
+    mu = E / 2. / (1. + nu)
+    K = lmbda + 2.0 * mu / 3.0
+
+    expected_values = {ElasticModuli.YoungsModulus: E,
+                       ElasticModuli.PoissonRatio: nu,
+                       ElasticModuli.ShearModulus: mu,
+                       ElasticModuli.BulkModulus: K,
+                       ElasticModuli.FirstLameParameter: lmbda}
+
+    tol = 5.0e-13
+    computed_values = compute_elasticity_coefficients(E=E, nu=nu)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(E=E, K=K)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(E=E, G=mu)
+    compare_dicts(expected_values, computed_values, tol=tol)
+    computed_values = compute_elasticity_coefficients(E=E, mu=mu)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(E=E, lmbda=lmbda)
+    compare_dicts(expected_values, computed_values, tol=tol)
+    computed_values = compute_elasticity_coefficients(E=E, firstLame=lmbda)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(nu=nu, K=K)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(nu=nu, G=mu)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(nu=nu, lmbda=lmbda)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(lmbda=lmbda, mu=mu)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(lmbda=lmbda, K=K)
+    compare_dicts(expected_values, computed_values, tol=tol)
+
+    computed_values = compute_elasticity_coefficients(mu=mu, K=K)
+    compare_dicts(expected_values, computed_values, tol=tol)
 
 
 if __name__ == "__main__":
     test_boundary_normal()
+    test_elasticity_coefficients()
