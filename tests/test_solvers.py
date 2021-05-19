2c8d65bd73fba05147d232160545e8659b21ad5a
Sebastian Glane <glane@tu-berlin.de>
2021-05-19 09:04:59 +0200

    Simplified tests

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -8,43 +8,26 @@ from elastic_solver import TractionBCType
 import dolfin as dlfn
 
 
-class ClampedBarTest(LinearElasticProblem):
-    def __init__(self, n_points, main_dir=None):
-        super().__init__(main_dir)
-
-        self._n_points = n_points
-        self._problem_name = "ClampedBarTest"
-
-        self.set_parameters(E=210.0, nu=0.3)
-
-    def setup_mesh(self):
-        # create mesh
-        self._mesh, self._boundary_markers = hyper_cube(2, self._n_points)
-
-    def set_boundary_conditions(self):
-        # velocity boundary conditions
-        self._bcs = [(DisplacementBCType.fixed, BoundaryMarkers.left.value, None),
-                     (DisplacementBCType.constant, BoundaryMarkers.right.value, (0.1, 0.0))]
-
-    def postprocess_solution(self):
-        # compute stresses
-        stress_tensor = self._compute_stress_tensor()
-        # add stress components to the field output
-        component_indices = []
-        for i in range(self.space_dim):
-            for j in range(i, self.space_dim):
-                component_indices.append((i+1, j+1))
-        for k, stress in enumerate(stress_tensor.split()):
-            stress.rename("S{0}{1}".format(*component_indices[k]), "")
-            self._add_to_field_output(stress)
-
-
 class TensileTest(LinearElasticProblem):
-    def __init__(self, n_points, main_dir=None):
+    def __init__(self, n_points, main_dir=None, bc_type="floating"):
         super().__init__(main_dir)
-
+        
+        assert isinstance(n_points, int)
+        assert n_points > 0
         self._n_points = n_points
-        self._problem_name = "TensileTest"
+        
+        assert isinstance(bc_type, str)
+        assert bc_type in ("floating", "clamped", "clamped_free", "pointwise")
+        self._bc_type = bc_type
+
+        if self._bc_type == "floating":
+            self._problem_name = "TensileTest"
+        elif self._bc_type == "clamped":
+            self._problem_name = "TensileTestClamped"
+        elif self._bc_type == "clamped_free":
+            self._problem_name = "TensileTestClampedFree"
+        elif self._bc_type == "pointwise":
+            self._problem_name = "TensileTestPointwise"
 
         self.set_parameters(E=210.0, nu=0.3)
 
@@ -54,10 +37,19 @@ class TensileTest(LinearElasticProblem):
 
     def set_boundary_conditions(self):
         # boundary conditions
-        # gamma = dlfn.CompiledSubDomain("near(x[0], 0.0) && near(x[1], 0.0) && on_boundary")
-        self._bcs = [(DisplacementBCType.fixed_component, BoundaryMarkers.left.value, 0, None),
-                     (DisplacementBCType.fixed_component, BoundaryMarkers.bottom.value, 1, None),
-                     (DisplacementBCType.constant_component, BoundaryMarkers.right.value, 0, 0.1)]
+        self._bcs = []
+        if self._bc_type == "floating":
+            self._bcs.append((DisplacementBCType.fixed_component, BoundaryMarkers.left.value, 0, None))
+            self._bcs.append((DisplacementBCType.fixed_component, BoundaryMarkers.bottom.value, 1, None))
+            self._bcs.append((DisplacementBCType.constant_component, BoundaryMarkers.right.value, 0, 0.1))
+        elif self._bc_type == "clamped":
+            self._bcs.append((DisplacementBCType.fixed, BoundaryMarkers.left.value, None))
+            self._bcs.append((DisplacementBCType.constant, BoundaryMarkers.right.value, (0.1, 0.0)))
+        elif self._bc_type == "clamped_free":
+            self._bcs.append((DisplacementBCType.fixed, BoundaryMarkers.left.value, None))
+            self._bcs.append((DisplacementBCType.constant_component, BoundaryMarkers.right.value, 0, 0.1))
+        elif self._bc_type == "pointwise":
+            raise NotImplementedError()
 
     def postprocess_solution(self):
         # compute stresses
@@ -80,12 +72,22 @@ class TensileTest(LinearElasticProblem):
                 print("({0},{1}) : {2:8.2e}".format(i, j, avg_stress))
 
 
-class DisplacementControlledShearTest(LinearElasticProblem):
-    def __init__(self, n_points, main_dir=None):
+class ShearTest(LinearElasticProblem):
+    def __init__(self, n_points, main_dir=None, bc_type="displacement"):
         super().__init__(main_dir)
 
+        assert isinstance(n_points, int)
+        assert n_points > 0
         self._n_points = n_points
-        self._problem_name = "DisplacementControlledShearTest"
+        
+        assert isinstance(bc_type, str)
+        assert bc_type in ("displacement", "traction")
+        self._bc_type = bc_type
+
+        if self._bc_type == "displacement":
+            self._problem_name = "DisplacementControlledShearTest"
+        elif self._bc_type == "traction":
+            self._problem_name = "TractionControlledShearTest"
 
         self.set_parameters(E=210.0, nu=0.3)
 
@@ -95,50 +97,15 @@ class DisplacementControlledShearTest(LinearElasticProblem):
 
     def set_boundary_conditions(self):
         # boundary conditions
-        # gamma = dlfn.CompiledSubDomain("near(x[0], 0.0) && near(x[1], 0.0) && on_boundary")
-        self._bcs = [(DisplacementBCType.fixed, BoundaryMarkers.bottom.value, None),
-                     (DisplacementBCType.constant, BoundaryMarkers.top.value, (0.1, 0.0))]
+        self._bcs = []
+        self._bcs.append((DisplacementBCType.fixed, BoundaryMarkers.bottom.value, None)),
 
-    def postprocess_solution(self):
-        # compute stresses
-        stress_tensor = self._compute_stress_tensor()
-        # add stress components to the field output
-        component_indices = []
-        for i in range(self.space_dim):
-            for j in range(i, self.space_dim):
-                component_indices.append((i+1, j+1))
-        for k, stress in enumerate(stress_tensor.split()):
-            stress.rename("S{0}{1}".format(*component_indices[k]), "")
-            self._add_to_field_output(stress)
-        # compute volume average of the stress tensor
-        dV = dlfn.Measure("dx", domain=self._mesh)
-        V = dlfn.assemble(dlfn.Constant(1.0) * dV)
-        print("Volume-averaged stresses: ")
-        for i in range(self.space_dim):
-            for j in range(self.space_dim):
-                avg_stress = dlfn.assemble(stress_tensor[i,j] * dV) / V
-                print("({0},{1}) : {2:8.2e}".format(i, j, avg_stress))
+        if self._bc_type == "displacement":
+            self._bcs.append((DisplacementBCType.constant, BoundaryMarkers.top.value, (0.1, 0.0)))
 
-
-class TractionControlledShearTest(LinearElasticProblem):
-    def __init__(self, n_points, main_dir=None):
-        super().__init__(main_dir)
-
-        self._n_points = n_points
-        self._problem_name = "TractionControlledShearTest"
-
-        self.set_parameters(E=210.0, nu=0.3)
-
-    def setup_mesh(self):
-        # create mesh
-        self._mesh, self._boundary_markers = hyper_cube(2, self._n_points)
-
-    def set_boundary_conditions(self):
-        # boundary conditions
-        # gamma = dlfn.CompiledSubDomain("near(x[0], 0.0) && near(x[1], 0.0) && on_boundary")
-        self._bcs = [(DisplacementBCType.fixed, BoundaryMarkers.bottom.value, None),
-                     (DisplacementBCType.fixed_component, BoundaryMarkers.top.value, 1, None),
-                     (TractionBCType.constant_component, BoundaryMarkers.top.value, 0, 0.1)]
+        elif self._bc_type == "traction":
+            self._bcs.append((DisplacementBCType.fixed_component, BoundaryMarkers.top.value, 1, None))
+            self._bcs.append((TractionBCType.constant_component, BoundaryMarkers.top.value, 0, 0.1))
 
     def postprocess_solution(self):
         # compute stresses
@@ -233,22 +200,16 @@ class BCFunctionTest(LinearElasticProblem):
             self._add_to_field_output(stress)
 
 
-def test_block_test():
-    block_test = ClampedBarTest(25)
-    block_test.solve_problem()
-
-
 def test_tensile_test():
-    tensile_test = TensileTest(25)
-    tensile_test.solve_problem()
+    for bc_type in ("floating", "clamped", "clamped_free"):
+        tensile_test = TensileTest(25, bc_type=bc_type)
+        tensile_test .solve_problem()
 
 
 def test_shear_test():
-    shear_test_disp = DisplacementControlledShearTest(25)
-    shear_test_disp.solve_problem()
-    shear_test_traction = TractionControlledShearTest(25)
-    shear_test_traction.solve_problem()
-
+    for bc_type in ("displacement", "traction"):
+        shear_test = ShearTest(25, bc_type=bc_type)
+        shear_test.solve_problem()
 
 def test_body_force():
     body_force_test = BodyForceTest(25)
@@ -261,7 +222,6 @@ def test_bc_function():
 
 
 if __name__ == "__main__":
-    test_block_test()
     test_tensile_test()
     test_shear_test()
     test_body_force()
