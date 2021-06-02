ea28f624578bb5d0c5d4ca69e40cd59aa205b931
sebglane <seb_glane@gmx.de>
2021-06-02 20:54:23 +0200

    BDF formulas upto 2nd order and 2nd derivatives

--- source/bdf_time_stepping.py
+++ source/bdf_time_stepping.py
@@ -9,19 +9,21 @@ import math
 class BDFTimeStepping(DiscreteTime):
     """Time stepping using adaptive backward differentiation formulas for
     derivative of degree upto three.
-    
+
     """
     def __init__(self, start_time, end_time, order=2, desired_start_time_step=0.0):
         super().__init__(start_time, end_time, desired_start_time_step)
 
         assert isinstance(order, int)
         assert order > 0
+        if order > 2:
+            raise NotImplementedError()
         self._order = order
 
-        self._coefficients_changed = True
+        self._coefficients_changed = {1: True, 2: True}
 
-        # initialize time step ratio
-        self._omega = -1.0
+        # initialize list of time step ratios
+        self._omega = [1.0, 1.0]
 
         # initialize coefficient dictionary
         self._alpha = dict()
@@ -34,19 +36,6 @@ class BDFTimeStepping(DiscreteTime):
         self._alpha[2][0] = 1.0   # initialize with values of
         self._alpha[2][1] = -2.0  # a first order scheme
         self._alpha[2][2] = 1.0
-        # coefficients of the third derivative
-        self._alpha[3] = [0.0, ] * (self._order + 3)
-        self._alpha[3][0] = 1.0   # initialize with values of
-        self._alpha[3][1] = -3.0  # a first order scheme
-        self._alpha[3][2] = 3.0
-        self._alpha[3][3] = -1.0
-        # coefficients of the fourth derivative
-        self._alpha[4] = [0.0, ] * (self._order + 4)
-        self._alpha[4][0] = 1.0   # initialize with values of
-        self._alpha[4][1] = -4.0  # a first order scheme
-        self._alpha[4][2] = 6.0
-        self._alpha[4][3] = -4.0
-        self._alpha[4][4] = 1.0
 
     def restart(self):
         """
@@ -54,10 +43,10 @@ class BDFTimeStepping(DiscreteTime):
         """
         super().restart()
 
-        self._coefficients_changed = True
+        self._coefficients_changed = {1: True, 2: True}
 
         # initialize time step ratio
-        self._omega = -1.0
+        self._omega = [1.0, 1.0]
 
         # coefficients of the first derivative
         self._alpha[1] = [0.0, ] * (self._order + 1)
@@ -68,89 +57,51 @@ class BDFTimeStepping(DiscreteTime):
         self._alpha[2][0] = 1.0   # initialize with values of
         self._alpha[2][1] = -2.0  # a first order scheme
         self._alpha[2][2] = 1.0
-        # coefficients of the third derivative
-        self._alpha[3] = [0.0, ] * (self._order + 3)
-        self._alpha[3][0] = 1.0   # initialize with values of
-        self._alpha[3][1] = -3.0  # a first order scheme
-        self._alpha[3][2] = 3.0
-        self._alpha[3][3] = -1.0
-        # coefficients of the fourth derivative
-        self._alpha[4] = [0.0, ] * (self._order + 4)
-        self._alpha[4][0] = 1.0   # initialize with values of
-        self._alpha[4][1] = -4.0  # a first order scheme
-        self._alpha[4][2] = 6.0
-        self._alpha[4][3] = -4.0
-        self._alpha[4][4] = 1.0
 
     def update_coefficients(self):
         # do not change coefficients in the first step
-        if (self._step_number == 0):
+        if (self.step_number == 0):
             return
-        # compute new time step ratio
+        # compute new time step ratios
         omega = self.get_next_step_size() / self.get_previous_step_size()
         assert math.isfinite(omega)
         assert omega > 0.0
-        
-        #TODO: Capture OMEGA
-        Omega = omega
-        #TODO: Capture XI
-        Xi = Omega
-        
-        # check if time step ratio has changed and update the IMEX coefficients
+        Omega = self._omega[0]
+        assert Omega > 0.0
+
+        # check if time step ratio has changed and update the coefficients
         # from the first to the second time step the coefficients change because
         # of switching from a first order to a second order scheme
-        if self._omega == omega and self._step_number > 1:
-            self._coefficients_changed = False
-            return
-        else:
-            self._omega = omega
+        if self._order == 1:
+            if (self._omega[0] == omega and self._step_number > 1):
+                for key in self._coefficients_changed.keys():
+                    self._coefficients_changed[key] = False
+                return
+            else:
+                self._omega[0] = omega
+                self._omega[1] = Omega
 
-            a, b = self._imex_parameters
-            
-            if self._order == 1:
                 # coefficients of the first derivative
                 self._alpha[1][0] = 1.0
                 self._alpha[1][1] = -1.0
+                self._coefficients_changed[1] = False
                 # coefficients of the second derivative
                 self._alpha[2][0] = 2.0 * omega / (1.0 + omega)
                 self._alpha[2][1] = -2.0 * omega
                 self._alpha[2][2] = 2.0 * omega * omega / (1.0 + omega)
-                # coefficients of the third derivative
-                num = 6.0 * omega**2 * Omega
-                denom = (1.0 + omega) * (1.0 + Omega + omega * Omega)
-                self._alpha[3][0] = num / denom
-                num = -6.0 * omega**2 * Omega
-                denom = 1.0 + Omega
-                self._alpha[3][1] = num / denom
-                num = 6.0 * omega**3 * Omega
-                denom = 1.0 + omega
-                self._alpha[3][2] = num / denom
-                num = -6.0 * omega**3 * Omega**3
-                denom = (1.0 + Omega) * (1.0 + Omega + omega * Omega)
-                self._alpha[3][3] = num / denom
-                # coefficients of the fourth derivative
-                num = 24.0 * Xi * omega**3 * Omega**2
-                denom = (1.0 + omega) * (1.0 + Omega + omega * Omega) *\
-                    (1.0 + Xi * (1.0 + Omega + omega * Omega))
-                self._alpha[4][0] = num / denom
-                num = -24.0 * Xi * omega**3 * Omega**2
-                denom = (1.0 + Omega) * (1.0 + Xi + Xi * Omega)
-                self._alpha[4][1] = num / denom
-                num = 24.0 * Xi * omega**4 * Omega**2
-                denom = 1.0 + Xi + omega + Xi * omega
-                self._alpha[4][2] = num / denom
-                num = -24.0 * Xi * omega**4 * Omega**4
-                denom = (1.0 + Omega) * (1.0 + Omega + omega * Omega)
-                self._alpha[4][3] = num / denom
-                num = 24.0 * Xi**4 * omega**4 * Omega**4
-                denom = (1.0 + Xi) * (1.0 + Xi + Xi * Omega) *\
-                    (1.0 + Xi * (1.0 + Omega + omega * Omega))
-                self._alpha[4][4] = num / denom
-            elif self._order == 2:
+                self._coefficients_changed[2] = True
+        elif self._order == 2:
+            if (self._omega[0] == omega and self._omega[1] == Omega and
+                    self._step_number > 1):
+                for key in self._coefficients_changed.keys():
+                    self._coefficients_changed[key] = False
+                return
+            elif (self._omega[0] == omega and self._omega[1] != Omega and
+                      self._step_number > 1):
+                self._omega[1] = Omega
+
                 # coefficients of the first derivative
-                self._alpha[1][0] = (1.0 + 2.0 * omega) / (1.0 + omega)
-                self._alpha[1][1] = -(1.0 + omega)
-                self._alpha[1][2] = omega * omega / (1.0 + omega)
+                self._coefficients_changed[1] = False
                 # coefficients of the second derivative
                 num = 2.0 * omega * (1.0 + (2.0 + 3.0 * omega) * Omega)
                 denom = (1.0 + omega) * (1.0 + Omega + omega * Omega)
@@ -164,99 +115,63 @@ class BDFTimeStepping(DiscreteTime):
                 num = -2.0 * omega**2 * (1.0 + 2.0 * omega) * Omega**3
                 denom = (1.0 + Omega) * (1.0 + Omega + omega * Omega)
                 self._alpha[2][3] = num / denom
-                # coefficients of the third derivative
-                num = 6.0 * omega**2 * Omega *\
-                        (1.0 + Xi * (2.0 + (3.0 + 4.0 * omega) * Omega))
-                denom = (1.0 + omega) * (1.0 + Omega + omega * Omega) *\
-                        (1.0 + Xi * (1.0 + Omega + omega * Omega))
-                self._alpha[3][0] = num / denom
-                
-                num = -6.0 * omega**2 * Omega *\
-                        (1.0 + Xi * (2.0 + 3.0 * (1.0 + omega) * Omega))
-                denom = (1.0 + Omega) * (1.0 + Xi + Xi * Omega)
-                self._alpha[3][1] = num / denom
-                num = 6.0 * omega**3 * Omega *\
-                        (1.0 + Xi * (2.0 + (2.0 + 3.0 * omega) * Omega))
-                denom = (1.0 + Xi) * (1.0 + omega)
-                self._alpha[3][2] = num / denom
-                num = -6.0 * omega**3 * Omega**3 *\
-                        (1.0 + Xi + Xi * (2.0 + 3.0 * omega) * Omega)
-                denom = (1.0 + Omega) * (1.0 + Omega + omega * Omega)
-                self._alpha[3][3] = num / denom
-                num = 6.0 * Xi**4 * omega**3 * Omega**3 *\
-                        (1.0 + (2.0 + 3.0 * omega) * Omega)
-                denom = (1.0 + Xi) * (1.0 + Xi + Xi * Omega) *\
-                        (1.0 + Xi * (1.0 + Omega + omega * Omega))
-                self._alpha[3][4] = num / denom
-                # coefficients of the fourth derivative
-                self._alpha[4] = None
-            elif self._order == 3:
+                self._coefficients_changed[2] = True
+
+            else:
+                self._omega[0] = omega
+                self._omega[1] = Omega
+
                 # coefficients of the first derivative
-                num = 1.0 + Omega + 3.0 * omega**2 * Omega + omega * (2. + 4.0 * Omega)
-                denom = (1.0 + omega) * (1.0 + Omega + omega * Omega)
-                self._alpha[1][0] = num / denom
-                num = -(1.0 + omega) * (1.0 + Omega + omega * Omega)
-                denom = (1.0 + Omega)
-                self._alpha[1][1] = num / denom
-                num = omega**2 * (1.0 + Omega + omega * Omega)
-                denom = (1 + omega)
-                self._alpha[1][2] = num / denom
-                num = omega**2 * (1.0 + omega) * Omega**3
-                denom = (1.0 + Omega) * (1.0 + Omega + omega * Omega)
-                self._alpha[1][3] = num / denom
+                self._alpha[1][0] = (1.0 + 2.0 * omega) / (1.0 + omega)
+                self._alpha[1][1] = -(1.0 + omega)
+                self._alpha[1][2] = omega * omega / (1.0 + omega)
+                self._coefficients_changed[1] = True
                 # coefficients of the second derivative
-                num = 2.0 * omega * (1.0 + Xi + (2.0 + 3.0 * omega) * Omega 
-                                     + Xi * (4.0 + 6.0 * omega) * Omega 
-                                     + Xi * (3.0 + 9.0 * omega + 6.0 * omega**2) * Omega**2)
-                denom = (1.0 + omega) * (1.0 + Omega + omega * Omega) *\
-                        (1.0 + Xi * (1.0 + Omega + omega * Omega))
+                num = 2.0 * omega * (1.0 + (2.0 + 3.0 * omega) * Omega)
+                denom = (1.0 + omega) * (1.0 + Omega + omega * Omega)
                 self._alpha[2][0] = num / denom
-                
-                num = -2.0 * omega * (1.0 + Xi + 2.0 * (1.0 + omega) * Omega 
-                                      + 4.0 * Xi * (1.0 + omega) * Omega 
-                                      + 3.0 * Xi * (1.0 + omega)**2 * Omega**2)
-                denom = (1.0 + Omega) * (1.0 + Xi + Xi * Omega)
+                num = -2.0 * omega * (1.0 + 2.0 * (1.0 + omega) * Omega)
+                denom = 1.0 + Omega
                 self._alpha[2][1] = num / denom
-                num = 2.0 * omega**2 * (1.0 + Xi + Omega
-                                        + 2.0 * omega * Omega
-                                        + Xi * (2.0 + 4.0 * omega) * Omega
-                                        + Xi * (1.0 + 4.0 * omega + 3.0 * omega**2) * Omega**2)
-                denom = (1.0 + Xi) * (1.0 + omega)
+                num = 2.0 * omega**2 * (1.0 + Omega + 2.0 * omega * Omega)
+                denom = 1.0 + omega
                 self._alpha[2][2] = num / denom
-                num = -2.0 * omega**2 * Omega**3 *\
-                        (1.0 + 2.0 * omega + Xi * (1.0 + Omega + 3.0 * omega**2 * Omega
-                                                   + omega * (2.0 + 4.0 * Omega)))
+                num = -2.0 * omega**2 * (1.0 + 2.0 * omega) * Omega**3
                 denom = (1.0 + Omega) * (1.0 + Omega + omega * Omega)
                 self._alpha[2][3] = num / denom
-                num = 2.0 * Xi**4 * omega**2 * Omega**3 *\
-                        (1.0 + Omega + 3.0 * omega**2 * Omega + omega * (2.0 + 4.0 * Omega))
-                denom = (1.0 + Xi) * (1.0 + Xi + Xi * Omega) *\
-                        (1.0 + Xi * (1.0 + Omega + omega * Omega))
-                self._alpha[2][4] = num / denom
-                # coefficients of the third derivative
-                self._alpha[3] = None
-                # coefficients of the fourth derivative
-                self._alpha[4] = None
-            self._coefficients_changed = True
+                self._coefficients_changed[2] = True
 
     def print_coefficients(self):
-        print("+-" + "-+-".join(4 * (12 * "-", )) + "-+")
-        print("| {:12} | {:12} | {:12} | {:12} |"
-              .format("coefficient", "n + 1", "n", "n - 1"))
-        print("| {:12} | {:12.2e} | {:12.2e} | {:12.2e} |"
-              .format("alpha", *self._alpha))
-        print("| {:12} | ".format("beta") + 12 * " " +
-              " | {:12.2g} | {:12.2e} |".format(*self._beta))
-        print("| {:12} | {:12.2e} | {:12.2e} | {:12.2g} |"
-              .format("gamma", *self._gamma))
-        print("| {:12} | ".format("eta") + 12 * " " +
-              " | {:12.2g} | {:12.2e} |".format(*self._eta))
+        """Print the coefficients of the different derivatives to STDOUT."""
+        levels = ("n + 1", "n", "n - 1", "n - 2")
+        n_levels = 2 + self._order
+        n_columns = n_levels + 1
+        print("+-" + "-+-".join( n_columns * (12 * "-", )) + "-+")
+        print(("| {:12} | " + " | ".join(n_levels * ("{:12}", )))
+              .format("derivative", *levels[:n_levels]) + " |" )
+        for d, coeffs in self._alpha.items():
+            if d == 1:
+                d_str = "1st"
+            elif d == 2:
+                d_str = "2nd"
+            if len(coeffs) == n_levels:
+                line = "| {:12} | " + " | ".join(n_levels * ("{:12.2e}", ))
+                line = line.format(d_str, *coeffs)
+                line += " |"
+                print(line)
+            else:
+                line = "| {:12} | " + " | ".join(len(coeffs) * ("{:12.2e}", ))
+                line = line.format(d_str, *coeffs)
+                line += " | "
+                line += " | ".join((n_levels - len(coeffs)) * (12 * " ", ))
+                line += " |"
+                print(line)
+        print("+-" + "-+-".join(n_columns * (12 * "-", )) + "-+")
 
-    @property
-    def alpha(self, derivative):
-        assert derivative in (1, 2, 3, 4)
-        return self._alpha[derivative]
+    def coefficients(self, derivative):
+        assert derivative in (1, 2)
+        return tuple(self._alpha[derivative])
 
-    @property
-    def coefficients_changed(self):
-        return self._coefficients_changed
+    def coefficients_changed(self, derivative):
+        assert derivative in (1, 2)
+        return self._coefficients_changed[derivative]
